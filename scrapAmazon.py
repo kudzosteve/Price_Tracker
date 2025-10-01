f02709ef5ef6300b7dbc9f9b39eec8bb50b15c70
kudzosteve <kudzosteve24726@gmail.com>
2025-09-30 23:26:35 -0500

    Update and rename scrap_amzn.py to scrapAmazon.py

---
+++ scrapAmazon.py
@@ -0,0 +1,85 @@
+import os, time, smtplib
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+PRODUCTS = {
+    "product1": {
+        "the_url": "",
+        "target_price": 0.00,
+    },
+    "product2": {
+        "the_url": "",
+        "target_price": 0.00,
+    },
+    "product3": {
+        "the_url": "",
+        "target_price": 0.00,
+    },
+}
+
+""" Import environment variables """
+load_dotenv()
+smtp_addr = os.getenv("SMTP_ADDRESS")
+email_addr = os.getenv("EMAIL_ADDRESS")
+email_passwd = os.getenv("EMAIL_PASSWORD")
+
+""" Open browser, get product data from website, and send notification according to logic """
+def scrap_amazon(the_url:str, target_price: float):
+    # 1. Launch Chrome browser in headless mode
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
+
+    driver = webdriver.Chrome(options=options)
+    driver.get(url=the_url)
+    driver.implicitly_wait(5)
+
+    # 2. Fetch the name and current pricing of the product
+    try:
+        print("🔃 Retrieving the name and current price of the product")
+
+        product_name = driver.find_element(By.ID, value="titleSection").text
+        price_dollars = driver.find_element(By.CLASS_NAME, value="a-price-whole").text
+        price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction").text
+        current_price_str = price_dollars + '.' + price_cents
+        current_price = float(current_price_str)
+
+        # 3. Send an email if the price has dropped
+        if current_price > target_price:
+            print(f"❌ No lower price found for {product_name}")
+
+        else:
+            print(f"✅ Lower price found. Sending notification...")
+            message = f"Product: {product_name}\n\
+            Current price: ${current_price}\n\
+            Link: {the_url}"
+
+            # 4. Establish a secure SMTP connection, log user credentials and send email
+            with smtplib.SMTP(smtp_addr, port=587) as connection:
+                connection.starttls()
+                connection.login(user=email_addr, password=email_passwd)
+                connection.sendmail(
+                    from_addr=email_addr,
+                    to_addrs=email_addr,
+                    msg=f"Subject: Your item has a lower price\n{message}".encode("utf-8"),
+                )
+            print("✅ Email has been sent.")
+
+    except Exception as e:
+        print(f"⚠️ Error: {e}")
+
+    finally:
+        print("Closing browser...")
+        driver.quit()  # Close the browser
+
+def main():
+    for key in list(PRODUCTS.keys()):
+        scrap_amazon(the_url=PRODUCTS[key]["the_url"],
+                     target_price=PRODUCTS[key]["target_price"])
+        time.sleep(3)   # Wait for 3 second before checking the next item
+
+if __name__ == "__main__":
+    main()
