9b8464f2dfb5afdf7cb1699586e8a4df287a9bd5
kudzosteve <kudzosteve24726@gmail.com>
2025-09-30 23:15:31 -0500

    Update scrapMicrocenter.py

--- scrap_microcntr.py
+++ scrap_microcntr.py
@@ -1,13 +1,23 @@
-import os
-import time
-import smtplib
-import sqlite3
+import os, time, smtplib
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-DB_FILE = os.path.join(os.getcwd(), "products.db")
+# Add as many products as needed
+PRODUCTS = {
+    "product1": {
+        "the_url": "",
+        "target_price": 0.00,
+    },
+    "product2": {
+        "the_url": "",
+        "target_price": 0.00,
+    },
+    "product3": {
+        "the_url": "",
+        "target_price": 0.00,
+}
 
 """ Import environment variables """
 load_dotenv()
@@ -15,81 +25,59 @@ smtp_addr = os.getenv("SMTP_ADDRESS")
 email_addr = os.getenv("EMAIL_ADDRESS")
 email_passwd = os.getenv("EMAIL_PASSWORD")
 
-""" Retrieve data from the database """
-def fetch_db_data():
-    # Connect to the database
-    conn = sqlite3.connect(DB_FILE)
-    cursor = conn.cursor()
-
-    results = cursor.execute("SELECT nickname, product_url, target_price FROM microcenter_products")
-
-    # Store the data in a list
-    products_data = [result for result in results.fetchall()]
-
-    # Commit changes and disconnect from the database
-    conn.commit()
-    conn.close()
-    return products_data  # Return the list
-
 """ Open browser, get product data from website, and send notification according to logic """
-def scrap_microcenter(nickname:str, product_link:str, target_price:float):
-    # 1. Start Chrome browser with headless behavior
+def scrap_microcenter(the_url:str, target_price: float):
+    # 1. Launch Chrome browser in headless mode
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
 
     driver = webdriver.Chrome(options=options)
-    driver.get(url=product_link)
-    time.sleep(5)
+    driver.get(url=the_url)
+    driver.implicitly_wait(5)
 
     # 2. Fetch the name and current pricing of the product
     try:
-        print("[*] Retrieving the name and current price of the product")
+        print("🔃 Retrieving the name and current price of the product")
 
-        item_name = driver.find_element(By.CLASS_NAME, value="product-header").text
-        big_price = driver.find_element(By.CLASS_NAME, value="big-price").text  # Value should be a string
-
-        # Remove the $ sign from the price and convert the numbers from string to float
-        pricing = float(big_price.split('$')[1])
+        product_name = driver.find_element(By.CLASS_NAME, value="product-header").text
+        current_price_str = driver.find_element(By.CLASS_NAME, value="big-price").text
+        current_price = float(current_price_str.split("$")[1])
 
         # 3. Send an email if the price has dropped
-        if pricing > target_price:
-            print(f"[X] No lower price found for {nickname}. Moving on to next product if any.")
+        if current_price > target_price:
+            print(f"❌ No lower price found for {product_name}")
 
         else:
-            print(f"[*] {nickname} has a lower price. Sending notification...")
-            message = f"Product: {item_name}\n \
-                    Current price: ${pricing}\n \
-                    Link: {product_link}\n"
+            print(f"✅ Lower price found. Sending notification...")
+            message = f"Product: {product_name}\n\
+            Current price: ${current_price}\n\
+            Link: {the_url}"
 
-            # 4. Establish a secure SMTP connection and log user credentials
+            # 4. Establish a secure SMTP connection, log user credentials and send email
             with smtplib.SMTP(smtp_addr, port=587) as connection:
                 connection.starttls()
                 connection.login(user=email_addr, password=email_passwd)
-
                 connection.sendmail(
                     from_addr=email_addr,
                     to_addrs=email_addr,
-                    msg=f"Subject: {nickname} has a lower price\n\
-                                                {message}".encode("utf-8")
+                    msg=f"Subject: Your item has a lower price\n{message}".encode("utf-8"),
                 )
-            print("[+] Email has been sent.")
+            print("✅ Email has been sent.")
+
     except Exception as e:
         print(f"⚠️ Error: {e}")
 
     finally:
-        driver.quit()   # Close the browser
+        print("Closing browser...")
+        driver.quit()  # Close the browser
+
+def main():
+    for key in list(PRODUCTS.keys()):
+        scrap_microcenter(the_url=PRODUCTS[key]["the_url"],
+                          target_price=PRODUCTS[key]["target_price"])
+        time.sleep(3)   # Wait for 3 second before checking the next item
 
 if __name__ == "__main__":
-    print("[*] Opening web browser...")
-
-    products_data = fetch_db_data()
-    for i in range(0, len(products_data)):
-        scrap_microcenter(products_data[i][0],  # Product's nickname
-                          products_data[i][1],  # Product URL
-                          products_data[i][2]  # Product's target price
-        )
-        print()
-        time.sleep(5)
-    print("[*] Closing web browser.")
+    main()
